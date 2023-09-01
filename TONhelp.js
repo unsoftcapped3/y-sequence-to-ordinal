5293882b8ae381c55f15c16524418837608602bd
solarzone1010 <139612896+solarzone1010@users.noreply.github.com>
2023-09-01 16:32:52 +1200

    Add files via upload

---
+++ TONhelp.js
@@ -0,0 +1,87 @@
+let d=document.getElementById("output");
+let q=document.getElementById("expr");
+let t='';
+let p=true;
+function spans(x){return document.getElementById(x.toString());}
+function c(){
+  let Ac=0;
+  let Cc=0;
+  let ee=''
+  for(i of t){
+    if(i=='C'){Cc++;}
+    else{Ac++;}
+  }
+  p=Ac-Cc==1;
+  if(!p){ee=((Cc>Ac)?'Too many':'Not enough')+' C\'s'}
+  else{
+    let c=0;
+    for(i of [...t.split('').keys()]){
+      c+=t[i]=='C'?-1:1;
+      if(c<1){p=0;ee='Mismatched C\'s';break;}
+    }
+  }
+  if(p){
+    for(i of [...Array(t.length).keys()]){spans(i).setAttribute('style','color:#000');}
+  }
+  else{
+    for(i of [...Array(t.length).keys()]){spans(i).setAttribute('style','color:#888');}
+  }
+  document.getElementById('error').innerHTML=ee;
+}
+function f(){
+  t=q.value;
+  d.innerHTML='';
+  [...t.split('').keys()].forEach(i=>{d.innerHTML+=`<span id='${i}'>${t[i]}</span>`;});
+  c();
+}
+function h(x){
+  if(t[x]!='C'){return[x,x];}
+  let s2=x;
+  let c=0;
+  while(c!=1){
+    s2--;
+    c+=t[s2]=='C'?-1:1;
+  }
+  let e2=s2;
+  c=0;
+  while(c!=1){
+    e2--;
+    c+=t[e2]=='C'?-1:1;
+  }
+  return [s2,e2]
+}
+function g(x){
+  for(i of [...t.split('').keys()]){spans(i).setAttribute('style','color:#000');}
+  if(p){
+    if(x<0){return;}
+    spans(x).setAttribute('style','color:#f80')
+    let s2=x-1;
+    if(t[x]=='C'){
+      let s2=h(x)[0];
+      let e2=h(x)[1];
+      for(i of [...Array(x).keys()].slice(s2)){spans(i).setAttribute('style','color:#f00');}
+      for(i of [...Array(s2).keys()].slice(e2)){spans(i).setAttribute('style','color:#0f0');}
+    }
+    p=[x];
+    v=x;
+    for(i of[0,1,2]){
+      if(v==t.length-1){break;}
+      while(1){
+        v++;
+        if(t[v]!='C'){continue;}
+        if(h(v)[1]<=p.at(-1)){
+          p.push(v);
+          break;
+        }
+      }
+    }
+    console.log(p)
+    console.log(' ')
+    let col=['#0dd','#0bb','#088']
+    //console.log(p,[...p.keys()].toReversed().slice(0,-1))
+    for(i of [...p.keys()].toReversed().slice(0,-1)){for(j of [...Array(p[i]+1).keys()].slice(h(p[i])[1])){if(j>x||j<h(x)[1]){spans(j).setAttribute('style',`color:${col[i-1]}`);}}}
+  }
+}
+
+document.getElementById('disp').onclick=f
+d.addEventListener('mouseover',(e)=>{id=e.target.tagName.toLowerCase()=='span'?Number(e.target.id):-1;g(id);});
