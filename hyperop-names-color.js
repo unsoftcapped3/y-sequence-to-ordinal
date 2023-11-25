5281d5492236a7942f91795b8f118865ba1ddc8e
solarzone1010 <139612896+solarzone1010@users.noreply.github.com>
2023-11-25 18:20:12 +1300

    Add files via upload

---
+++ hyperop-names-color.js
@@ -0,0 +1,200 @@
+// from https://naruyoko.github.io/tests/notation/psiletter/Q.html
+// (slightly modified)
+const ZERO=0;
+const ONE={pow:ZERO,coef:1,add:ZERO};
+const TWO={pow:ZERO,coef:2,add:ZERO};
+const OMEGA={pow:ONE,coef:1,add:ZERO};
+const colors = [,'#0000ff','#00d800','#ff0000','#00c0ff','#e0d000','#d000d0','#6000ff'];
+let apos='’'
+function add(a,b){
+  if(a===ZERO){return b;}
+  if(a.add==ZERO){return {pow:a.pow,coef:a.coef,add:b};}
+  else{return {pow:a.pow,coef:a.coef,add:add(a.add,b)};}
+}
+
+function colorize(x,c){
+  return `<span style="color:${colors[c]}">${x}</span>`;
+}
+
+function parseOrdinal(s,index,allowLowPriorityOperations){
+  if (index===undefined) index=0;
+  if (allowLowPriorityOperations===undefined) allowLowPriorityOperations=true;
+  var quit=false;
+  var ord=ZERO;
+  while (!quit&&index<s.length){
+    /** @type {Ordinal} */
+    var pow;
+    /** @type {number} */
+    var coef;
+    if (s[index]=="w"||s[index]=="ω"){
+      if (s[index+1]=="^"){
+        if (s[index+2]=="("||s[index+2]=="{"){
+          var result=parseOrdinal(s,index+3,true);
+          if (result===null) return null;
+          pow=result[0];
+          index=result[1];
+        }else{
+          var result=parseOrdinal(s,index+2,false);
+          if (result===null) return null;
+          pow=result[0];
+          index=result[1];
+        }
+      }else{
+        pow=ONE;
+        index++;
+      }
+      if (allowLowPriorityOperations&&s[index]=="*"){
+        index++;
+        var coefstart=index;
+        while ("0123456789".indexOf(s[index])!=-1) index++;
+        coef=parseInt(s.substring(coefstart,index));
+      }else coef=1;
+    }else if ("0123456789".indexOf(s[index])!=-1){
+      pow=ZERO;
+      var coefstart=index;
+      while ("0123456789".indexOf(s[index])!=-1) index++;
+      coef=parseInt(s.substring(coefstart,index));
+    }
+    if (ord instanceof Object||ord===0) ord=add(ord,{pow:pow,coef:coef,add:ZERO});
+    if (allowLowPriorityOperations&&index<s.length){
+      if (s[index]==")"||s[index]=="}"){
+        index++;
+        quit=true;
+      }else if (s[index]=="+") index++;
+      else return null;
+    }else quit=true;
+  }
+  return [ord,index];
+}
+
+function _add(c,p,q){
+  if(!c.match(/<span/)){return p+c+q;}
+  return c.slice(0,28)+p+c.slice(28,-7)+q+'</span>'
+}
+
+function prefix(n,m){
+  let v=''
+  let w=''
+  let o=''
+  if(m==0){
+    let X=['','hen','di','tri','tetr','pent','hex','hept','oct','enne'];
+    let Y=['','dec','icos','triacont','tetracont','pentacont','hexacont','heptacont','octacont','enneacont'];
+    if(n<10){return X[n];}
+    if(n==23){return 'tricos'}
+    if(n%10==2){return 'do'+Y[Math.floor(n/10)];}
+    if(n%10==3){return 'tri'+Y[Math.floor(n/10)];}
+    return X[n%10]+(((n>10&&n<20)||(n>29&&n%10>0))?'a':'')+Y[Math.floor(n/10)];
+  }
+  if(m==1){
+    let X=['','kal','mej','gij','ast','lun','ferm','jov','sol','bet','gloc','gax','sup','vers','mult','met','xev','hyp','omniv','out'];
+    let Y=['','','barr','gic','asc','luc','ferc','joc','solc','bec'];
+    if(n<20){v=X[n];}
+    else if(n>39&&n<50){v=X[n%10]+'asc';}
+    else{v=X[n%10].slice(0,-1)+Y[Math.floor(n/10)];}
+  }
+  else{
+    let X=['','hep','ott','net','det','unt','ent','fit','syt','bront'];
+    let Y=['','gep','am','hap','kir','pij','sag','pec','nis','zot']
+    if(m<10){v=X[m];}
+    else if(m==22){v='otam';}
+    else if(m%10==2){v='o'+X[Math.floor(m/10)];}
+    else if(m>19&&m<30){v=X[m%10]+'am';}
+    else{v=X[m%10].slice(0,-1)+Y[Math.floor(m/10)];}
+    if(n>1){w=prefix(n,1).slice(28,-7)+(m%10==6?'i':'e');}
+  }
+  if(m<6){o=colorize(w+v,m);}
+  else if(m>=6&&w){o=colorize(w,6)+colorize(v,7);}
+  else{o=colorize(v,7);}
+  return o;
+}
+
+function leftNF(a){
+  if(a===ZERO){return [];}
+  return [[a.coef,a.add]].concat(leftNF(a.pow));
+}
+
+function toString(a){
+  if(a===ZERO){return '0';}
+  if(a.pow===ZERO){return a.coef.toString();}
+  let m='';
+  if(a.pow===ONE){m='ω';}
+  else if(a.pow.pow===ZERO||(a.pow.coef===1&&a.pow.add===ZERO)){m=`ω^${toString(a.pow)}`;}
+  else{m=`ω^(${toString(a.pow)})`}
+  if(a.coef>1){
+    if(a.pow===ONE){m+=a.coef;}
+    else{m+='*'+a.coef;}
+  }
+  if(a.add!=ZERO){m+='+'+toString(a.add);}
+  return m;
+}
+
+function toHTML(a,n=3){
+  if(!n){return toString(a);}
+  if(a===ZERO){return '0';}
+  if(a.pow===ZERO){return a.coef.toString();}
+  let m='';
+  if(a.pow===ONE){m='ω';}
+  else{m=`ω<sup>${toHTML(a.pow,n-1)}</sup>`;}
+  if(a.coef>1){m+=a.coef;}
+  if(a.add!=ZERO){m+='+'+toString(a.add);}
+  return m;
+}
+
+function name(a,t=0){
+  if(typeof(a)!='object'){a=parseOrdinal(a.toString())[0];}
+  let A=leftNF(a);
+  let x='_'
+  for(let i=0;i<A.length-1;i++){
+    if(A[i][0]==1&&A[i][1]=='0'){delete A[i];}
+  }
+  let m=0;
+  let r=[];
+  for(let i=0;i<A.length;i++){
+    if(!A[i]){continue;}
+    let y='_'
+    let l=''
+    if(i+1==A.length||A[i][0]>1){
+      let z=A.slice(i+1,-1).map(x=>(x[0])).every(x=>(x==1))?'':'`'
+      if(A[i][0]==2&&i+t==0){y=`di${z}_`;}
+      else if(A[i][0]==3&&i+t==0){y=`tri${z}_`;}
+      else{y=_add(prefix(A[i][0],i+t),'','a'+z)+'_'};
+    }
+    if(A[i][1]!=ZERO){y+=(i==A.length-2?'':'-')+name(A[i][1],i+t);}
+    if(i-m>1&&A.slice(0,i).some(x=>x[1]!=ZERO)&&!r.includes(m)){
+      r.push(m);
+      if(m==0){y+=apos+'o';}
+      if(m==1){y+=apos+'i';}
+      if(m==2){y+=colorize(apos+'ok',3);}
+      if(m>2){y+=prefix(0,m).slice(0,28)+apos+prefix(0,m).slice(28,-8)+'k</span>';}
+    }
+    x=x.replace('_',y);
+    if(A[i][1]){m=i;}
+  }
+  x=x.replace('_','');
+  x=x.replaceAll(`a</span>${apos}o`,'</span>'+colorize('o',1));
+  x=x.replaceAll(`a</span>${apos}i`,'</span>'+colorize('i',2));
+  x=x.replaceAll(/a<span style="color:#[0-9a-f]{6}">a/g,x=>x.slice(1));
+  x=x.replaceAll(/a<\/span><span style="color:#[0-9a-f]{6}">a/g,x=>x.slice(8));
+  return x;
+}
+
+function hyperop(a){
+  if(typeof(a)!='object'){a=parseOrdinal(a.toString())[0];}
+  if(a==ONE){return 'addition';}
+  if(a==TWO){return 'multiplication';}
+  if(a=={pow:0,coef:3,add:0}){return 'exponentiation';}
+  if(a.pow==ZERO){return prefix(parseInt(toString(a)),0)+'ation';}
+  return name(a)+'tion';
+}
+
+function hyperop_(a){
+  let x=hyperop(a);
+  return x;
+}
+
+function calculate(){
+  document.getElementById('output2').innerHTML=`[${toHTML(parseOrdinal(document.getElementById('input').value)[0])}] is called`
+  document.getElementById('output').innerHTML=hyperop_(document.getElementById('input').value);
+}
+document.getElementById('input').value='w^w^(w+1)+1'
+calculate();
